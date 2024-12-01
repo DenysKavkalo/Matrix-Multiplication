8d8e81e1bfa8e294c62878d8d7b26e31198734a9
Denys Kavkalo <117307592+DenysKavkalo@users.noreply.github.com>
2024-12-01 23:25:17 +0000

    Add files via upload

---
+++ TASK 3. Vectorized and Parallel Matrix Multiplication/Matrix Multiplication/src/main/java/org/example/MatrixMultiplicationMain.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.openjdk.jmh.Main;
+
+public class MatrixMultiplicationMain {
+    public static void main(String[] args) throws Exception {
+        // This will run the JMH benchmark
+        Main.main(args);
+    }
+}
---
+++ TASK 3. Vectorized and Parallel Matrix Multiplication/Matrix Multiplication/target/generated-sources/annotations/org/example/jmh_generated/BenchmarkMatrixMultiplication_jmhType.java
@@ -0,0 +1,4 @@
+package org.example.jmh_generated;
+public class BenchmarkMatrixMultiplication_jmhType extends BenchmarkMatrixMultiplication_jmhType_B3 {
+}
+
